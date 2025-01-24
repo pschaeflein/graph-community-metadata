084afd8d9f33a45732f9756a75c1d035e5202a1a
Paul Schaeflein <paul@schaeflein.net>
2025-01-24 08:01:16 -0600

    project update

---
+++ build-client.ps1
@@ -0,0 +1,34 @@
+Write-Host "Building client..."
+
+$openapiFolder = "./docs"
+$apiclientFolder = "../graph-community-spclient/codegen/lib/apiclient"
+
+tsp compile src/main.tsp --emit @typespec/openapi3
+
+# Fix OpenApi doc
+./fixOpenApi.ps1
+
+# Generate client
+kiota generate -l csharp -d "$openapiFolder/openapi.json" -c SPClient -n Graph.Community -o $apiclientFolder --cc --co --ebc
+
+# Rename-Item -Path ../codegen/lib/apiclient/Item/_api/MicrosoftSharepointUtilitiesWebTemplateExtensionsSiteScriptUtilityGetSiteDesignMetadata -NewName SiteScriptUtilityGetSiteDesignMetadata
+# Rename-Item -Path ../codegen/lib/apiclient/Item/_api/SiteScriptUtilityGetSiteDesignMetadata/MicrosoftSharepointUtilitiesWebTemplateExtensionsSiteScriptUtilityGetSiteDesignMetadataRequestBuilder.cs -NewName SiteScriptUtilityGetSiteDesignMetadataRequestBuilder.cs
+
+
+# Remove the 'With___' bits from /web/GetFileBy...
+$WebRequestBuilder = "$apiclientFolder/Item/_api/Web/WebRequestBuilder.cs"
+(Get-Content $WebRequestBuilder) -replace "GetFileByIdWithId\(Guid\? id\)", "GetFileById(Guid? id)" | Set-Content $WebRequestBuilder
+(Get-Content $WebRequestBuilder) -replace "public global::Graph.Community.Item._api.Web.GetFileByServerRelativePathWithPath.GetFileByServerRelativePathWithPathRequestBuilder GetFileByServerRelativePathWithPath", "public global::Graph.Community.Item._api.Web.GetFileByServerRelativePathWithPath.GetFileByServerRelativePathWithPathRequestBuilder GetFileByServerRelativePath" | Set-Content $WebRequestBuilder
+
+# Update URL templates
+$ListsItemRequestBuilder = "$apiclientFolder/Item/_api/Web/Lists/Item/ListsItemRequestBuilder.cs"
+(Get-Content $ListsItemRequestBuilder) -replace "/_api/web/lists/{id}", "/_api/web/lists/getById('{id}')" | Set-Content $ListsItemRequestBuilder
+
+$SitePagesRequestBuilder = "$apiclientFolder/Item/_api/SitePages/SitePagesRequestBuilder.cs"
+(Get-Content $SitePagesRequestBuilder) -replace "/_api/SitePages", "/_api/SitePages/Pages" | Set-Content $SitePagesRequestBuilder
+$SitePagesItemRequestBuilder = "$apiclientFolder\Item\_api\SitePages\Item\SitePagesItemRequestBuilder.cs"
+(Get-Content $SitePagesItemRequestBuilder) -replace "/_api/SitePages/{id}", "/_api/SitePages/Pages({id})" | Set-Content $SitePagesItemRequestBuilder
+
+dotnet test ../graph-community-spclient/graph-community-spclient.sln
+
+Write-Host "Complete."
